5dd44251456b946a14c4a9a455d016d1fc21af7f
givemeapassword <vt070502@yandex.ru>
2023-12-07 11:52:36 +0300

    добавление болванки характеристик с помощью rv и childRv

--- app/src/main/java/com/example/rp_cyberpunk_list/expand_rv_characteristic/CharacteristicViewAdapter.kt
+++ app/src/main/java/com/example/rp_cyberpunk_list/expand_rv_characteristic/CharacteristicViewAdapter.kt
@@ -1,28 +1,43 @@
 package com.example.rp_cyberpunk_list.expand_rv_characteristic
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.rp_cyberpunk_list.databinding.FragmentCharacteristicsSkillsBinding
+import com.example.rp_cyberpunk_list.R
+import com.example.rp_cyberpunk_list.databinding.CharacterisiticItemBinding
 
 class CharacteristicViewAdapter(private val characteristicItemList: List<Characteristics>) :
     RecyclerView.Adapter<CharacteristicViewAdapter.ParentRecyclerViewHolder>() {
-        private lateinit var binding: FragmentCharacteristicsSkillsBinding
 
     inner class ParentRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(){
+        private val binding = CharacterisiticItemBinding.bind(itemView)
+        fun bind(characteristicsItem: Characteristics){
+            binding.apply {
+                parentTitleTv.text = characteristicsItem.name
+                childRecyclerView.setHasFixedSize(true)
+                childRecyclerView.layoutManager = LinearLayoutManager(itemView.context)
+            }
+            val adapter = SkillsViewAdapter(characteristicsItem.skillItemList)
+            binding.childRecyclerView.adapter = adapter
 
+            val isExpandable = characteristicsItem.expandable
+            binding.childRecyclerView.visibility = if(isExpandable) View.VISIBLE else View.GONE
+            binding.constraintLayout.setOnClickListener {
+                characteristicsItem.expandable = !characteristicsItem.expandable
+                notifyItemChanged(position)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParentRecyclerViewHolder {
-        binding = FragmentCharacteristicsSkillsBinding.bind(parent)
-        return ParentRecyclerViewHolder(binding.root)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.characterisitic_item,parent,false)
+        return ParentRecyclerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ParentRecyclerViewHolder, position: Int) {
-        val parentItem = characteristicItemList[position]
-
+        holder.bind(characteristicItemList[position])
     }
     override fun getItemCount(): Int {
         return characteristicItemList.size
--- app/src/main/java/com/example/rp_cyberpunk_list/expand_rv_characteristic/SkillsViewAdapter.kt
+++ app/src/main/java/com/example/rp_cyberpunk_list/expand_rv_characteristic/SkillsViewAdapter.kt
@@ -1,27 +1,32 @@
 package com.example.rp_cyberpunk_list.expand_rv_characteristic
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.rp_cyberpunk_list.R
 import com.example.rp_cyberpunk_list.databinding.SkillItemBinding
 
 class SkillsViewAdapter(private val skillList: List<Skills>) :
         RecyclerView.Adapter<SkillsViewAdapter.ChildViewHolder>() {
-            private lateinit var binding: SkillItemBinding
 
         inner class ChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val binding = SkillItemBinding.bind(itemView)
+            fun bind(skills: Skills) {
+                binding.cardSkillText.text = skills.name
+            }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildViewHolder {
-            binding = SkillItemBinding.bind(parent)
-            return ChildViewHolder(binding.root)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.skill_item,parent,false)
+            return ChildViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ChildViewHolder, position: Int) {
+            holder.bind(skillList[position])
         }
 
         override fun getItemCount(): Int {
             return skillList.size
         }
     }
-}
